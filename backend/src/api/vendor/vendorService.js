312e35e251e01bbc7919c02ae21321975eb3a8da
AlanLucasSC <AlanLucasSC2011@gmail.com>
2018-04-17 13:47:06 -0400

    New route on Vendor

--- backend/src/api/vendor/vendorService.js
+++ backend/src/api/vendor/vendorService.js
@@ -22,4 +22,24 @@ Vendor.route('products', function(req, res, next){
     });
 });
 
+Vendor.route('products/:name', function(req, res, next){
+    console.log(req.params.name)
+    Vendor.find(function (err, query) {
+        if (err) {
+            res.status(500)
+            res.send("Erro interno do servidor")
+        }
+        else {
+            res.status(200)
+            array = []
+            query.forEach(function(vendor){
+                vendor.products.forEach(function(products){
+                    array.push(products)
+                })
+            })
+            res.send(array)
+        }
+    });
+});
+
 module.exports = Vendor
