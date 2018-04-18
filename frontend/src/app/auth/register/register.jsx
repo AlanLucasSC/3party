1402ffb0ac56a44bcdc7f43400cefec443da1164
AlanLucasSC <AlanLucasSC2011@gmail.com>
2018-04-18 14:56:01 -0400

    Register updated

--- frontend/src/app/auth/register/register.jsx
+++ frontend/src/app/auth/register/register.jsx
@@ -4,25 +4,27 @@ export default props => (
     <div className="modal fade" id="myModal" role="dialog" aria-hidden="true">
         <div className="modal-dialog">
             <div className="modal-content">
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-default" data-dismiss="modal">
-                        <i className="fa fa-times" aria-hidden="true"></i>
-                    </button>
-                </div>
+                <button type="button" className="btn btn-default" data-dismiss="modal">
+                    <i className="fa fa-times" aria-hidden="true"></i>
+                </button>
                 <div className="modal-body">
                     <form action="/action_page.php">
                         <div className="form-group">
-                            <label htmlFor="email">Email address:</label>
+                            <label htmlFor="email">Usuário:</label>
+                            <input type="user" className="form-control" id="user"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email:</label>
                             <input type="email" className="form-control" id="email"/>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="pwd">Password:</label>
+                            <label htmlFor="pwd">Senha:</label>
                             <input type="password" className="form-control" id="pwd"/>
                         </div>
                         <div className="checkbox">
-                            <label><input type="checkbox"/> Remember me</label>
+                            <button type="submit" id="submit" className="btn btn-outline-warning">Cadastrar</button>
+                            <label><input type="checkbox"/> Lembre-me</label>
                         </div>
-                        <button type="submit" className="btn btn-default">Submit</button>
                     </form>
                 </div>
             </div>
