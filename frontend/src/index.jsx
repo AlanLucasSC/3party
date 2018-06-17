cee3fdd6032ca5a71ded170f69abeed7d6640584
Alan Lucas Silva de Castro <alanlucassc2011@gmail.com>
2018-06-16 20:06:12 -0400

    State saved and loaded from LocalStorage

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -14,10 +14,36 @@ import thunk from 'redux-thunk'
 import App from './app/app'
 import reducers from './app/shared/store/reducers'
 
+//Save state on localstorage
+function savaToLocalStorage( state ) {
+    try {
+        const serializedState = JSON.stringify(state)
+        localStorage.setItem('state', serializedState)
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+//Load state from localstorage
+function loadFromLocalStorage() {
+    try {
+        const serializedState = localStorage.getItem('state')
+        if(serializedState === null) return undefined
+        return JSON.parse(serializedState)
+    } catch(e) {
+        console.log(e)
+        return undefined
+    }
+}
+
+const persistedState = loadFromLocalStorage();
+
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
         && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, persistedState, devTools)
+
+store.subscribe(() => savaToLocalStorage(store.getState()))
 
 ReactDOM.render(
     <Provider store={ store }>
